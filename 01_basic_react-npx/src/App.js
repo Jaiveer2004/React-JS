e76c222d7d6a774f4177aee8bb56002352066152
JaiveerSingh2004 <jaiveer2104.be23@chitkara.edu.in>
2024-11-01 21:49:01 +0530

    Added 1st and 2nd Lec

---
+++ 01_basic_react-npx/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Chai aur Code | Jaiveer Singh</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basic_react-vite/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h3>Hello, Chai aur Code pe apka swagat hai</h3>
+    );
+}
+
+export default Chai;
